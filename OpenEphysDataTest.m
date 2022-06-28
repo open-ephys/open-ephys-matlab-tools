6610e615639309450349c4c6b7b69130f7c1d989
Pavel Kulik <pavelkulik@hotmail.com>
2022-06-28 09:22:27 -0700

    Remove test scripts

--- OpenEphysDataTest.m
+++
@@ -1,7 +0,0 @@
-clear;
-close all;
-clc;
-
-path = '/home/pavel/open-ephys/Binary_OpenEphys_Recording';
-
-session = Session(path);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
