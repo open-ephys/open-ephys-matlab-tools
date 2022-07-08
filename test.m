dc481aff4856928ec5cb8881b654c5d4986fda0f
Pavel Kulik <pavelkulik@hotmail.com>
2022-07-08 09:55:36 -0700

    Update test code

--- test.m
+++ test.m
@@ -1,29 +1,46 @@
 % "Import" matlab-tools
 addpath(genpath("."));
 
-% Define a path to the recorded data
-path = 'C:\Users\Pavel\OneDrive\Documents\Open Ephys\2022-06-30_20-13-10'; %SourceSim
-%path = 'C:\Users\Pavel\OneDrive\Documents\Open Ephys\2022-06-30_18-37-31'; %FileReader
+% Define a base path to the recorded data
+dataPath = '/Users/pavelkulik/Documents/Open Ephys';
 
-% Create a session (loads all data at the path)
+% Get the path to the most recent recording
+path = fullfile(dataPath, Utils().getLatestRecording(dataPath).name);
+
+% Create a session (loads all data from the most recent recording)
 session = Session(path);
 
 % Get the number of record nodes for this session
 nRecordNodes = length(session.recordNodes);
 
-% Iterate over the record nodes to access their data
+% Iterate over the record nodes to access data
 for i = 1:nRecordNodes
-    
+
     node = session.recordNodes{i};
-    
-    event_streams = node.recordings{1,1}.ttlEvents.keys();
-    if length(event_streams) > 0
-        some_stream = event_streams{1};
-        some_events = node.recordings{1,1}.ttlEvents(some_stream);
+
+    % Get the first recording 
+    recording = node.recordings{1};
+
+    % Iterate over all data streams in the recording 
+    streamNames = recording.continuous.keys();
+    for j = 1:length(streamNames)
+
+        stream = recording.continuous(streamNames{j});
+
+        % Plot the data from the current stream
+        % figure; plot(1:length(stream.samples), stream.samples);
+       
+        % TODO: Get event data
+        event_streams = node.recordings{1,1}.ttlEvents.keys();
+        if length(event_streams) > 0
+            some_stream = event_streams{1};
+            some_events = node.recordings{1,1}.ttlEvents(some_stream);
+        end
+
     end
       
     %Events are stored as a pandas DataFrame equivalent for each stream
-    some_events.disp
+    %some_events.disp
     
 end
 
