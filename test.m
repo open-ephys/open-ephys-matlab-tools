b107364c6bdd70084ae56549c6db07aa64d11ed2
Pavel Kulik <pavelkulik@hotmail.com>
2022-07-19 19:29:26 -0700

    Update test script

--- test.m
+++ test.m
@@ -1,8 +1,13 @@
 % "Import" matlab-tools
-%addpath(genpath("."));
+addpath(genpath("."));
+
+% Define size for demo figure 
+FIGURE_X_SIZE = 1800;
+FIGURE_Y_SIZE = 1000;
 
 % Define a base path to the recorded data
 dataPath = '/Users/pavelkulik/Documents/Open Ephys';
+dataPath = 'C:\Users\Pavel\OneDrive\Documents\Open Ephys';
 
 % Get the path to the most recent recording
 path = fullfile(dataPath, Utils().getLatestRecording(dataPath).name);
@@ -18,8 +23,8 @@ for i = 1:nRecordNodes
 
     node = session.recordNodes{i};
 
-    f = figure(); colors = {'b', 'r'};
-    f.set('Position', [0 0 1800 1000]);
+    f = figure();
+    f.set('Position', [0 0 FIGURE_X_SIZE FIGURE_Y_SIZE]);
 
     for j = 1:length(node.recordings)
 
@@ -37,17 +42,23 @@ for i = 1:nRecordNodes
             data = recording.continuous(streamName);
     
             % 2. Plot the continuous data 
-            plot(data.timestamps, data.samples); hold on;
+            plot(data.timestamps, data.samples, 'LineWidth', 1.5); hold on;
            
-            % 3. Overlay any corresponding event data for this stream/recording
-            events = recording.ttlEvents(streamName);
-            if ~isempty(events)
-                for n = 1:length(events.channel)
-                    plot(events.timestamp(n), 1e4, 'r+'); hold on;
+            % 3. Overlay all available event data
+            eventProcessors = recording.ttlEvents.keys();
+            for p = 1:length(eventProcessors)
+                processor = eventProcessors{p};
+                events = recording.ttlEvents(processor);
+                if ~isempty(events)
+                    for n = 1:length(events.channel)
+                        if events.state(n) == 1
+                            line([events.timestamp(n), events.timestamp(n)], [-10000,10000], 'Color', 'b', 'LineWidth', 0.2);
+                        end
+                    end
                 end
             end
 
-            % TODO: Spike Data
+            % TODO: Demo Spike Data
             % electrodes = recording.spikes.keys();
     
         end
